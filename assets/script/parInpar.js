0e43a9357fa86fdf3bf65c1808c1d0c0d9d94bc5
dev-CristianD.Gonzalez <cristiand.gonzalez094@gmail.com>
2024-05-03 19:54:52 -0300

    Se finalizo el ejercicio de par o impar

---
+++ assets/script/parInpar.js
@@ -0,0 +1,7 @@
+let num1 = (prompt('Ingrese un número:'))
+
+if (num1 % 2 == 0) {
+    alert('El número ingresado es par.');
+} else if (num1 % 2 == 1) {
+    alert('El número ingresado es impar');    
+}
