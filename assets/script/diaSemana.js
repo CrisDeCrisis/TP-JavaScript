5754f1b7751e3d918ea44b368f44ee016bb5d117
dev-CristianD.Gonzalez <cristiand.gonzalez094@gmail.com>
2024-05-03 19:39:33 -0300

    Se finalizo el ejercicio de dias de la semana

---
+++ assets/script/diaSemana.js
@@ -0,0 +1,28 @@
+let dia = parseInt(prompt("Digíte un número de 1 a 7: "));
+
+switch (dia) {
+    case 1:
+        alert("Lunes");
+        break;
+    case 2:
+        alert("Martes");
+        break;
+    case 3:
+        alert("Miércoles");
+        break;
+    case 4:
+        alert("Jueves");
+        break;
+    case 5:
+        alert("Viernes");
+        break;
+    case 6:
+        alert("Sábado");
+        break;
+    case 7:
+        alert("Domingo");
+        break;
+    default:
+        alert("Número inválido");
+        break;
+}
